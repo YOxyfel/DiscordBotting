ad56fe530bd8855613a2a481b3671a18059ad24b
YOxyfel <125958938+YOxyfel@users.noreply.github.com>
2023-08-30 13:00:52 +0000

    Database Added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from replit import db
 import asyncio
 import json
 import os
@@ -7,10 +8,10 @@ import time
 import discord
 from discord.ext import commands
 import requests
+
 #from tictactoe import start_game
 
-intents = discord.Intents.default()
-intents.message_content = True
+db['key1'] = "value1"
 
 
 class MyClient(commands.Bot):
